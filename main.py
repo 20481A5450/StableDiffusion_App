551343ad7b02288cb424cf578c5aeb88cbef732e
Zohaib Shaik <20481A5450@gecgudlavallerumic.in>
2024-04-21 19:57:43 +0530

    created a basic flask application

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask, send_file
+from flask import Flask, render_template,send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/image')
-def get_image():
-    # Replace 'image.jpg' with the path to your generated image
-    image_path = 'image.jpg'
-    return send_file(image_path, mimetype='image/jpeg')
+# @app.route('/image')
+# def get_image():
+#     # Replace 'image.jpg' with the path to your generated image
+#     image_path = 'image.jpg'
+#     return send_file(image_path, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True)
